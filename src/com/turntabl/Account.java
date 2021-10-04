139375a3693f109b5b9379e8deb6febb38a6f9fc
princekk122 <awuahkarikari12@gmail.com>
2021-10-04 13:35:30 +0000

    first commit

---
+++ src/com/turntabl/Account.java
@@ -0,0 +1,13 @@
+package com.turntabl;
+
+public class Account {
+    private int tradeVal;
+
+    public int getTradeVal() {
+        return tradeVal;
+    }
+
+    public void setTradeVal(int tradeVal) {
+        this.tradeVal = tradeVal;
+    }
+}
---
+++ src/com/turntabl/Trade.java
@@ -0,0 +1,42 @@
+package com.turntabl;
+
+public class Trade {
+    private String id;
+    private String symbol;
+    private int quantity;
+    private double price;
+
+    public Trade(String id, String symbol, int quantity, double price) {
+        this(id, symbol, quantity);
+        this.price = price;
+    }
+
+    public Trade(String id, String symbol, int quantity) {
+        this.id = id;
+        this.symbol = symbol;
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0) {
+            this.price = price;
+            return;
+        }
+
+        throw new IllegalArgumentException("Price cannot be negative");
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Trade{" +
+                "id='" + id + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+}
